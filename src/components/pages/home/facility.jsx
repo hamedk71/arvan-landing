e519525e62fa4972a7806033b0347c6c04690c70
Hamed <hamed.karimian71@gmail.com>
2023-11-01 14:29:38 +0330

    updated facility pattern

--- src/components/pages/home/facility.jsx
+++ src/components/pages/home/facility.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import Pattern from './pattern'
 import Tab from './tab'
 import Subtitle from '@/components/utils/subTitle'
+import useDeviceType from '@/hooks/useDeviceType'
 
 export default function Facility() {
+  const device = useDeviceType()
   return (
     <section className=' py-24 md:py-32 relative flex flex-col items-center'>
-      <Pattern />
+      <Pattern length={device === 'mobile' ? 2 : 5} />
       <div className='w-full container font-bold'>
         <Subtitle>راهکارها</Subtitle>
         <h2 className='text-gray-900 text-[28px] leading-9 lg:text-[40px] mt-6'>
